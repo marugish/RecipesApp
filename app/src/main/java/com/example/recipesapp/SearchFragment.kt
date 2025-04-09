6f8e4a5852dc5b3d355026987d22ee98196101da
Анастасия Маругина <45240148+marugish@users.noreply.github.com>
2025-04-09 23:16:43 +0300

    v.1 Добавлены основные фрагменты и viewModel для них, а также .xml

---
+++ app/src/main/java/com/example/recipesapp/SearchFragment.kt
@@ -0,0 +1,27 @@
+package com.example.recipesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recipesapp.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    private lateinit var viewModel: SearchViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
