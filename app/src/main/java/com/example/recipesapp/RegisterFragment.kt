a1d70e9fdaf05df4a6ec831cf09d0b2f21ede25d
Анастасия Маругина <45240148+marugish@users.noreply.github.com>
2025-04-13 23:04:37 +0300

    v.1 Регистрация аккаунта в Firebase

--- app/src/main/java/com/example/recipesapp/RegisterFragment.kt
+++ app/src/main/java/com/example/recipesapp/RegisterFragment.kt
@@ -1,11 +1,15 @@
 package com.example.recipesapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.recipesapp.databinding.FragmentRegisterBinding
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 
 class RegisterFragment : Fragment() {
 
@@ -19,9 +23,69 @@ class RegisterFragment : Fragment() {
         return binding.root
     }
 
+    private var email: String = ""
+    private var password: String = ""
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val updateButtonState = {
+            val email = binding.mailEditText.text?.toString().orEmpty()
+            val password = binding.passwordEditText.text?.toString().orEmpty()
+            val confirmPassword = binding.confirmPasswordEditText.text?.toString().orEmpty()
+
+            binding.signupButton.isEnabled =
+                email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty() && confirmPassword == password
+        }
+
+        binding.mailEditText.addTextChangedListener(
+            afterTextChanged = { s ->
+                //email = s?.toString() ?: ""
+                updateButtonState()
+            }
+        )
+
+        binding.passwordEditText.addTextChangedListener(
+            afterTextChanged = { s ->
+                //password = s?.toString() ?: ""
+                updateButtonState()
+            }
+        )
+
+        binding.confirmPasswordEditText.addTextChangedListener(
+            afterTextChanged = { s ->
+                //confirmPassword = s?.toString() ?: ""
+                if (s?.toString() != binding.passwordEditText.text?.toString()) {
+                    binding.confirmPasswordEditText.error = getString(R.string.passwords_dont_match)
+                }
+                updateButtonState()
+            }
+        )
+
+        binding.signupButton.setOnClickListener {
+            val email = binding.mailEditText.text?.toString() ?: ""
+            val password = binding.passwordEditText.text?.toString() ?: ""
+
+            (activity as RootActivity).auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("myTest", "createUserWithEmail:success")
+                        //val user = auth.currentUser
+                        //updateUI(user)
+                        // Переходим на экран добавления фото
+                        findNavController().navigate(R.id.action_registerFragment_to_profileFragment)
+                    } else {
+                        Log.i("myTest", "createUserWithEmail:failure ${task.exception?.message.toString()}")
+                        Toast.makeText(requireContext(), "Authentication failed.", Toast.LENGTH_SHORT)
+                            .show()
+                        //updateUI(null)
+                    }
+                }
+        }
+
+        binding.signinButton.setOnClickListener {
+
+        }
 
     }
 }
--- app/src/main/java/com/example/recipesapp/RootActivity.kt
+++ app/src/main/java/com/example/recipesapp/RootActivity.kt
@@ -7,14 +7,19 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.recipesapp.databinding.ActivityRootBinding
 import com.google.android.gms.common.util.CollectionUtils.setOf
+import com.google.firebase.auth.FirebaseAuth
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
         val navController = navHostFragment.navController
