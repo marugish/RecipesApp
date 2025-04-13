eeebe2702dbdf5897a56606891fa6736fecb439c
Анастасия Маругина <45240148+marugish@users.noreply.github.com>
2025-04-13 23:06:42 +0300

    v.2 Реализация авторизации с помощью Firebase

--- app/src/main/java/com/example/recipesapp/LoginFragment.kt
+++ app/src/main/java/com/example/recipesapp/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.example.recipesapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.databinding.FragmentLoginBinding
 
@@ -23,12 +26,58 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val updateButtonState = {
+            val email = binding.mailEditText.text?.toString().orEmpty()
+            val password = binding.passwordEditText.text?.toString().orEmpty()
+
+            binding.signInButton.isEnabled = email.isNotEmpty() && password.isNotEmpty()
+        }
+
+        binding.mailEditText.addTextChangedListener(
+            afterTextChanged = { s ->
+                //email = s?.toString() ?: ""
+                updateButtonState()
+            }
+        )
+
+        binding.passwordEditText.addTextChangedListener(
+            afterTextChanged = { s ->
+                //password = s?.toString() ?: ""
+                updateButtonState()
+            }
+        )
+
+
         binding.signInButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_searchFragment)
+            val email = binding.mailEditText.text?.toString() ?: ""
+            val password = binding.passwordEditText.text?.toString() ?: ""
+
+            (activity as RootActivity).auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("myTest", "signInWithEmail:success")
+
+                        //val user = auth.currentUser
+                        //updateUI(user)
+
+                        findNavController().navigate(R.id.action_loginFragment_to_searchFragment)
+                    } else {
+                        Log.i("myTest", "signInWithEmail:failure ${task.exception?.message.toString()}")
+                        Toast.makeText(
+                            requireContext(), "Authentication failed.", Toast.LENGTH_SHORT
+                        ).show()
+
+                        //updateUI(null)
+                    }
+                }
+
         }
 
         binding.signUpButton.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
+
+
+
     }
 }
