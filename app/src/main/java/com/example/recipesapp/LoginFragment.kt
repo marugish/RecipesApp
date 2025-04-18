28f49930c2d821827f16230117dd574a135d0f5d
Анастасия Маругина <45240148+marugish@users.noreply.github.com>
2025-04-18 20:19:51 +0300

    v.7 Сделана авторизация через Google

--- app/src/main/java/com/example/recipesapp/LoginFragment.kt
+++ app/src/main/java/com/example/recipesapp/LoginFragment.kt
@@ -8,8 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.credentials.Credential
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.databinding.FragmentLoginBinding
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
 
@@ -81,5 +90,70 @@ class LoginFragment : Fragment() {
             findNavController().navigate(R.id.action_loginFragment_to_resetPasswordFragment)
         }
 
+        binding.googleLoginButton.setOnClickListener {
+
+            launchCredentialManager()
+        }
+
+    }
+
+    private fun launchCredentialManager() {
+        // Instantiate a Google sign-in request
+        val googleIdOption = GetGoogleIdOption.Builder()
+            // Your server's client ID, not your Android client ID.
+            .setServerClientId(getString(R.string.default_web_client_id))
+            // True - Only show accounts previously used to sign in.
+            .setFilterByAuthorizedAccounts(false) //true)
+            .build()
+
+        // Create the Credential Manager request
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+
+        lifecycleScope.launch {
+            try {
+                // Launch Credential Manager UI
+                val result = (activity as RootActivity).credentialManager.getCredential(
+                    context = requireContext(),
+                    request = request
+                )
+                // Extract credential from the result returned by Credential Manager
+                handleSignIn(result.credential)
+            } catch (e: GetCredentialException) {
+                Log.e("myTest", "Couldn't retrieve user's credentials: ${e.localizedMessage}")
+            }
+        }
+    }
+
+    private fun handleSignIn(credential: Credential) {
+        // Check if credential is of type Google ID
+        if (credential is CustomCredential && credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+            // Create Google ID Token
+            val googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+            // Sign in to Firebase with using the token
+            firebaseAuthWithGoogle(googleIdTokenCredential.idToken)
+        } else {
+            Log.w("myTest", "Credential is not of type Google ID!")
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        (activity as RootActivity).auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("myTest", "signInWithCredential:success")
+
+                    findNavController().navigate(R.id.action_loginFragment_to_searchFragment)
+                    //val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user
+                    Log.w("myTest", "signInWithCredential:failure", task.exception)
+                    //updateUI(null)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/recipesapp/RootActivity.kt
+++ app/src/main/java/com/example/recipesapp/RootActivity.kt
@@ -2,17 +2,30 @@ package com.example.recipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.view.isVisible
+import androidx.credentials.Credential
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.GetCredentialException
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.recipesapp.databinding.ActivityRootBinding
 import com.google.android.gms.common.util.CollectionUtils.setOf
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.launch
 
 class RootActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRootBinding
 
     lateinit var auth: FirebaseAuth
+    lateinit var credentialManager: CredentialManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +33,9 @@ class RootActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+
+        credentialManager = CredentialManager.create(this)
+
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
         val navController = navHostFragment.navController
@@ -40,4 +56,7 @@ class RootActivity : AppCompatActivity() {
         binding.bottomNavigationView.setupWithNavController(navController)
 
     }
+
+
+
 }
