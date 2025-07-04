f424ff9433f473d7a0393b51cbcd4f363492a2cc
marugish <marugish@yandex.ru>
2025-07-04 22:40:28 +0300

    v.2 Доделан экран деталей рецепта

---
+++ app/src/main/java/com/example/recipesapp/ui/search/IngredientsAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.recipesapp.ui.search
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recipesapp.databinding.IngredientItemBinding
+import com.example.recipesapp.domain.search.model.Ingredient
+
+@SuppressLint("NotifyDataSetChanged")
+class IngredientsAdapter(
+    private var ingredients: List<Ingredient> = emptyList(),
+    private val clickListener: (Ingredient) -> Unit
+) : RecyclerView.Adapter<IngredientsViewHolder>() {
+
+    fun updateIngredientsList(items: List<Ingredient>) {
+        ingredients = items
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IngredientsViewHolder {
+        val layoutInspector = LayoutInflater.from(parent.context)
+        return IngredientsViewHolder(
+            IngredientItemBinding.inflate(layoutInspector, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: IngredientsViewHolder, position: Int) {
+        holder.bind(ingredients[position])
+        holder.itemView.setOnClickListener {
+            clickListener(ingredients[position])
+            notifyDataSetChanged()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return ingredients.size
+    }
+}
--- app/src/main/java/com/example/recipesapp/ui/search/RecipeDetailsFragment.kt
+++ app/src/main/java/com/example/recipesapp/ui/search/RecipeDetailsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.navigation.fragment.findNavController
 import com.example.recipesapp.BaseFragment
 import com.example.recipesapp.databinding.FragmentRecipeDetailsBinding
 import com.example.recipesapp.presentation.search.RecipeDetailsViewModel
@@ -19,6 +21,17 @@ class RecipeDetailsFragment : BaseFragment<FragmentRecipeDetailsBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //popBackStack или navigateUp()
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        // Кнопка Back
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            findNavController().popBackStack()
+        }
+
     }
 
 }
