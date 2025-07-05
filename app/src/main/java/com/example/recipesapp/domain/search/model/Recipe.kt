14c6ba83e92baecd5c23faf259033b6bc1fd68d8
marugish <marugish@yandex.ru>
2025-07-05 17:14:56 +0300

    v.1 Вынесла ингредиенты в отдельный класс

--- app/src/main/java/com/example/recipesapp/domain/search/model/Recipe.kt
+++ app/src/main/java/com/example/recipesapp/domain/search/model/Recipe.kt
@@ -18,29 +18,3 @@ data class Recipe(
     "cookingMinutes": null,
    */
 )
-
-data class Ingredient(
-    val id: Int,
-    //val aisle: String,
-    val image: String,
-    //val consistency: String,
-    val name: String,
-    val nameClean: String,
-    val original: String,
-    val originalName: String,
-    val amount: Double,
-    val unit: String,
-    //val meta: List<String>,
-    /*val measures: Measures*/ // пока убрала
-)
-
-data class Measures(
-    val us: MeasureUnit,
-    val metric: MeasureUnit
-)
-
-data class MeasureUnit(
-    val amount: Double,
-    val unitShort: String,
-    val unitLong: String
-)
